7ae37ad52ed6bbf482f078ea86de7052499ca0a2
Samuel Caitano <83779077+SamuelCaitano@users.noreply.github.com>
2022-01-19 12:21:35 -0300

    Update index.php

--- 4_variaveis/exercicio_8/index.php
+++ 4_variaveis/exercicio_8/index.php
@@ -3,7 +3,7 @@
 $nome = "Samuel"; // string
 $idade = 22; // int
 $cidade = "Araraquara - SP"; // string
-$pais = ['Selma Maria', 'Armendo Caitano']; // array
+$pais = ['Selma', 'Armendo']; // array
 
 echo "Meu nome é $nome, tenho $idade anos e moro em $cidade, sou filho de $pais[0] e $pais[1]";
 
